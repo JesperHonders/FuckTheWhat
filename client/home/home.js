1f44262192ecb8444dfdecc4d013116e844b27ca
Joshua Onwezen <joshua_onwezen@hotmail.com>
2016-04-29 13:07:16 +0200

    Changes to timestamps on chart

--- client/home/home.js
+++ client/home/home.js
@@ -22,7 +22,7 @@ Template.home.rendered = function () {
 
     //set data - replace the random() function with api data later
     var data = {
-        labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
+        labels: Meteor.chartFunctions.addLabels(1),
         datasets: [
 //            {
 //                label: "Plein 1",
