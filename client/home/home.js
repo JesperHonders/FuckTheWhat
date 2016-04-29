697462fa25760f76c1035c521a76156080ee4ca5
Joshua Onwezen <joshua_onwezen@hotmail.com>
2016-04-29 12:04:12 +0200

    Merge changes

--- client/home/home.js
+++ client/home/home.js
@@ -1,9 +1,18 @@
 Template.home.helpers ({
   meldingen: function(){
-    return Meldingen.find({status: "done"});
+    return Meldingen.find({status: "done"}, {limit: 5});
+  },
+  events: function(){
+    return Events.find({}, {limit: 5});
   }
 })
 
+
+
+Template.home.onCreated(function bodyOnCreated() {
+  Meteor.subscribe('Meldingen');
+  Meteor.subscribe('Events');
+});
 
 Template.home.rendered = function () {    
     
